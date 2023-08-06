f45a088e38ea91eb59e7ac6f3196be1026f99b54
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2023-08-05 23:09:50 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AdminRouter, WebRouter } from "./router";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AdminRouter />
+      <WebRouter />
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/web/index.js
@@ -0,0 +1,5 @@
+export * from "./Home";
+export * from "./Blog";
+export * from "./Courses";
+export * from "./Contact";
+export * from "./Post";
