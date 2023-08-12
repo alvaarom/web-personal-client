2ac873b2e625858f1a1b6374a9d37712d0d2be23
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2023-08-12 09:41:50 -0300

    [ADD] menu section completed

---
+++ src/components/Admin/Menu/ListMenu/ListMenu.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Loader } from "semantic-ui-react";
+import { size, map } from "lodash";
+import { Menu } from "../../../../api";
+import { MenuItem } from "../MenuItem/MenuItem";
+
+const menuController = new Menu();
+
+export function ListMenu(props) {
+  const { active, reload, onReload } = props;
+  const [menus, setMenus] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await menuController.getMenu(active);
+        setMenus(response);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [active, reload]);
+
+  if (!menus) return <Loader active inline="centered" />;
+  if (size(menus) === 0) return "No hay ningun menu";
+
+  return map(menus, (menu) => (
+    <MenuItem key={menu._id} menu={menu} onReload={onReload} />
+  ));
+}
